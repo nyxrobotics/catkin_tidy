64c656fda71b7320139d9b62557adc27295eff91
Takaaki Numai <takaaki.numai01@g.softbank.co.jp>
2023-06-01 17:03:40 +0900

    Fix bug

--- clang_tidy/tidy.py
+++ clang_tidy/tidy.py
@@ -14,26 +14,27 @@ def runClangTidy(clang_binary, pkg_root, package, headers, sources, cfg, compile
     cmd = [clang_binary]
     cmd += ['-p={}'.format(compile_db)]
     # cmd += ['-header-filter={}/include/.*'.format(pkg_root)]
-    cmd += ['-header-filter={}/include'.format(pkg_root)]
-    # cmd += ['--warnings-as-errors={}/.*'.format(package)]
-    # cmd += ['--header-filter={}'.format(headers)]
-    # cmd += ['--header-filter=include/.*']
+    # cmd += ['-header-filter={}/include'.format(pkg_root)]
+    # cmd += ['-header-filter="{}"'.format(headers)]
+    # cmd += ['-header-filter=include/.*']
+    cmd += ['-header-filter={}'.format(pkg_root)]
     # cmd += ['-header-filter=.*']
     # cmd += ['-config={}'.format(cfg)]
     if fix:
-        cmd += ['--enable-check-profile']
-        cmd += ['--extra-arg=-std=c++11']
-        cmd += ['--fix']
-        cmd += ['--fix-errors']
-        cmd += ['--format-style=file']
-        # cmd += ['--quiet']
-        # cmd += ['--use-color']
-        cmd += ['--warnings-as-errors=*']
-        # cmd += ['--dump-config']
+        cmd += ['-checks=-*,google-readability-casting']
+        cmd += ['-enable-check-profile']
+        cmd += ['-extra-arg=-std=c++11']
+        cmd += ['-fix']
+        cmd += ['-fix-errors']
+        cmd += ['-format-style=file']
+        # cmd += ['-quiet']
+        # cmd += ['-use-color']
+        cmd += ['-warnings-as-errors=*']
+        # cmd += ['-dump-config']
     if export_file:
-        cmd += ['--export-fixes={}'.format(export_file)]
+        cmd += ['-export-fixes={}'.format(export_file)]
     cmd += sources
-    cmd += headers
+    # cmd += headers
     if dry_run:
         print(" ".join(cmd))
     else:
